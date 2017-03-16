9be20f76c956e97b7594be46089f23477a97aedd
Jaewon Noh <opqw987@gmail.com>
2017-03-16 18:03:32 +0900

    Hello world commit

--- StudyRxSwift/ViewController.swift
+++ StudyRxSwift/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Observable.just("Hello, World")
+            .map { value in
+                return "\(value) - Cool"
+            }
+            .subscribe { event in
+                switch event {
+                case .next(let value):
+                    print(value)
+                case .completed:
+                    print("complete")
+                case .error(let error):
+                    print(error)
+                }
+            }
+            .addDisposableTo(disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
